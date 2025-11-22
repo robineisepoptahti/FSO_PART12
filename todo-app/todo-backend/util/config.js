9ec00d0a289bb5ba06c4c3b3666863cf8621804f
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-11-22 19:30:07 +0200

    Added 12.16, frontend bound and coninerized

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,8 @@
 // Default to localhost:3456 for a local Mongo started by docker-compose
 // NOTE: don't include the "MONGO_URL=" prefix here — the value must be a Mongo connection string
-const MONGO_URL = process.env.MONGO_URL || "mongodb://mongo:27017/the_database";
-const REDIS_URL = process.env.REDIS_URL || "redis://redis:6379";
+const MONGO_URL =
+  process.env.MONGO_URL || "mongodb://localhost:27017/the_database";
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 module.exports = {
   MONGO_URL, //: 'mongodb://the_username:the_password@localhost:3456/the_database',
